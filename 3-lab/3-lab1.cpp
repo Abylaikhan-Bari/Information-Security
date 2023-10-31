522132e900d8d9b6300bd9575978063acf977ddc
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-31 09:36:27 +0600

    All labs completed

--- 3-lab/3-lab1.cpp
+++ 3-lab/3-lab1.cpp
@@ -2,42 +2,42 @@
 #include <string>
 #include <bitset>
 
-int main() {
-    
+using namespace std;
 
+int main() {
     int F1_bits, F2_bits, F3_bits, F4_bits;
 
     // Ввод разрядностей полей с консоли
-    std::cout << "Введите разрядность поля F1: ";
-    std::cin >> F1_bits;
+    cout << "Введите разрядность поля F1: ";
+    cin >> F1_bits;
 
-    std::cout << "Введите разрядность поля F2: ";
-    std::cin >> F2_bits;
+    cout << "Введите разрядность поля F2: ";
+    cin >> F2_bits;
 
-    std::cout << "Введите разрядность поля F3: ";
-    std::cin >> F3_bits;
+    cout << "Введите разрядность поля F3: ";
+    cin >> F3_bits;
 
-    std::cout << "Введите разрядность поля F4: ";
-    std::cin >> F4_bits;
+    cout << "Введите разрядность поля F4: ";
+    cin >> F4_bits;
 
     if (F1_bits + F2_bits + F3_bits + F4_bits != 32) {
-        std::cerr << "Сумма разрядностей полей не равна 32. Проверьте введенные значения.\n";
+        cerr << "Сумма разрядностей полей не равна 32. Проверьте введенные значения.\n";
         return 1;
     }
 
     //4F1A3E8D Запрос ввода пакета данных в виде 16-ричной символьной комбинации
-    std::string input;
-    std::cout << "Введите 32-разрядный пакет данных (16-ричная комбинация): ";
-    std::cin >> input;
+    string input;
+    cout << "Введите 32-разрядный пакет данных (16-ричная комбинация): ";
+    cin >> input;
 
     // Проверка на правильную длину входных данных
     if (input.length() != 8) {
-        std::cerr << "Неверная длина входных данных. Введите 8 символов.\n";
+        cerr << "Неверная длина входных данных. Введите 8 символов.\n";
         return 1;
     }
 
     // Преобразование входной строки в целое число
-    unsigned int packet = std::stoi(input, 0, 16);
+    unsigned int packet = stoi(input, 0, 16);
 
     // Создание масок для каждого поля
     unsigned int F1_mask = (1 << F1_bits) - 1;
@@ -56,12 +56,12 @@ int main() {
     int checksum = (F0 + F1 + F2 + F3 + F4) % 31;
 
     // Вывод результатов на экран
-    std::cout << "F0: " << F0 << std::endl;
-    std::cout << "F1: " << std::bitset<32>(F1) << std::endl;
-    std::cout << "F2: " << F2 << std::endl;
-    std::cout << "F3: " << std::bitset<32>(F3) << std::endl;
-    std::cout << "F4: " << std::bitset<32>(F4) << std::endl;
-    std::cout << "Контрольная сумма: " << checksum << std::endl;
+    cout << "F0: " << F0 << endl;
+    cout << "F1: " << bitset<32>(F1) << endl;
+    cout << "F2: " << F2 << endl;
+    cout << "F3: " << bitset<32>(F3) << endl;
+    cout << "F4: " << bitset<32>(F4) << endl;
+    cout << "Контрольная сумма: " << checksum << endl;
 
     return 0;
 }
